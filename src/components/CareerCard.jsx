85fe05a4a33ccf6c58bbf6dd5f0f9c164a596b10
Artt Vincii <otong123321@gmail.com>
2025-12-26 22:48:42 +0700

    feat: About CareerCard mobile

--- src/components/CareerCard.jsx
+++ src/components/CareerCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { GraduationCap, Briefcase } from "lucide-react";
 import ExperienceCard from "./ExperienceCard";
 
@@ -6,24 +6,24 @@ export default function CareerCard({ experiences, type }) {
   return (
     <section className="mt-7 pb-3">
       {type === "edu" ? (
-        <div className="mb-8">
-          <h3 className="text-2xl font-bold text-white flex items-center gap-2 font-mono tracking-tight">
-            <GraduationCap className="w-7 h-7 text-cyan-400" />
+        <div className="mb-6 md:mb-8">
+          <h3 className="text-xl md:text-2xl font-bold text-white flex items-center gap-2 font-mono tracking-tight">
+            <GraduationCap className="w-6 h-6 md:w-7 md:h-7 text-cyan-400" />
             Education
           </h3>
-          <p className="text-gray-400 mt-1 font-sans text-sm">
+          <p className="text-gray-400 mt-1 font-sans text-md">
             My educational journey.
           </p>
         </div>
       ) : null}
 
       {type === "work" ? (
-        <div className="mb-8">
-          <h3 className="text-2xl font-bold text-white flex items-center gap-2 font-mono tracking-tight">
-            <Briefcase className="w-7 h-7 text-cyan-400" />
+        <div className="mb-6 md:mb-8">
+          <h3 className="text-xl md:text-2xl font-bold text-white flex items-center gap-2 font-mono tracking-tight">
+            <Briefcase className="w-6 h-6 md:w-7 md:h-7 text-cyan-400" />
             Work Experience
           </h3>
-          <p className="text-gray-400 mt-1 font-sans text-sm">
+          <p className="text-gray-400 mt-1 font-sans text-md">
             My professional journey.
           </p>
         </div>
