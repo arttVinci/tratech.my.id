739e2c42e9a56483aebfc80b9b5322dfe14d2470
Artt Vincii <otong123321@gmail.com>
2026-01-03 00:25:30 +0700

    setup

--- server/internal/config/app.go
+++ server/internal/config/app.go
@@ -1,1 +1,15 @@
 package config
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"gorm.io/gorm"
+)
+
+type BootstrapConfig struct {
+	App   *fiber.App
+	DB    *gorm.DB
+	Viper *viper.Viper
+	Log   logrus.Logger
+}
--- server/internal/config/viper.go
+++ server/internal/config/viper.go
@@ -1,1 +1,27 @@
 package config
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+func newViper() *viper.Viper {
+	config := viper.New()
+
+	config.SetConfigName("config")
+	config.SetConfigType("json")
+	config.AddConfigPath("./../")
+	config.AddConfigPath("./")
+	err := config.ReadInConfig()
+
+	config.AutomaticEnv()
+	config.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+	}
+
+	return config
+}
