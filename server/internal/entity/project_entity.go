faa2c9fef371161626d8c823cfed8125cbbceb29
Artt Vincii <otong123321@gmail.com>
2026-01-07 01:51:39 +0700

    ProjectEntity

--- server/internal/entity/project_entity.go
+++ server/internal/entity/project_entity.go
@@ -1,1 +1,16 @@
 package entity
+
+type Project struct {
+	ID          uint   `gorm:"column:id;primaryKey"`
+	UserId      uint   `gorm:"column:id_user"`
+	Title       string `gorm:"column:title"`
+	ImageUrl    string `gorm:"column:image_url"`
+	Description string `gorm:"column:description"`
+	GithubUrl   string `gorm:"column:github_url"`
+	LiveUrl     string `gorm:"column:live_url"`
+	Challange   string `gorm:"column:challange"`
+	Solution    string `gorm:"column:solution"`
+	IsFeatured  bool   `gorm:"column:is_featured"`
+
+	Tags []string `gorm:"type:json;serializer:json"`
+}
