d16247ccfd89aafa0d108699e559c0cc400bd1ce
Artt Vincii <otong123321@gmail.com>
2026-01-11 00:33:00 +0700

    Achievement Controller

--- server/internal/delivery/http/achievement_controller.go
+++ server/internal/delivery/http/achievement_controller.go
@@ -1,1 +1,40 @@
 package http
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/sirupsen/logrus"
+	"tratech.my.id/server/internal/delivery/http/middleware"
+	"tratech.my.id/server/internal/model"
+	"tratech.my.id/server/internal/usecase"
+)
+
+type AchievementController struct {
+	UseCase *usecase.AchievementUseCase
+	Log     *logrus.Logger
+}
+
+func NewContactController(useCase *usecase.AchievementUseCase, log *logrus.Logger) *AchievementController {
+	return &AchievementController{
+		UseCase: useCase,
+		Log:     log,
+	}
+}
+
+func (c *AchievementController) Create(ctx *fiber.Ctx) error {
+	auth := middleware.GetUser(ctx)
+
+	request := new(model.CreateAchievementRequest)
+	if err := ctx.BodyParser(request); err != nil {
+		c.Log.WithError(err).Error("error parsing request body")
+		return fiber.ErrBadRequest
+	}
+	request.UserId = auth.ID
+
+	response, err := c.UseCase.Create(ctx.UserContext(), request)
+	if err != nil {
+		c.Log.WithError(err).Error("error creating achievement")
+		return err
+	}
+
+	return ctx.JSON(model.WebResponse[*model.AchievementResponse]{Data: response})
+}
--- server/internal/delivery/http/route/route.go
+++ server/internal/delivery/http/route/route.go
@@ -1,1 +1,13 @@
 package route
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"tratech.my.id/server/internal/delivery/http"
+)
+
+type RouteConfig struct {
+	App                   *fiber.App
+	UserController        *http.UserController
+	AchievementController *http.achievement
+	AuthMiddleware        fiber.Handler
+}
