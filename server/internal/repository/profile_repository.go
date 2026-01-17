e5aaef4ca4666c1122d0793ca18bd4afb5fa2610
Artt Vincii <otong123321@gmail.com>
2026-01-17 23:22:59 +0700

    Profile Repository

---
+++ server/internal/repository/profile_repository.go
@@ -0,0 +1,18 @@
+package repository
+
+import (
+	"gorm.io/gorm"
+	"tratech.my.id/server/internal/entity"
+)
+
+type ProfileRepository struct {
+	Repository[entity.Profile]
+}
+
+func NewProfileRepository() *ProfileRepository {
+	return &ProfileRepository{}
+}
+
+func (r *ProfileRepository) FindByIdAndUserId(db *gorm.DB, profile entity.Profile, id string, userId string) error {
+	return db.Where("id = ? AND user_id = ?", id, userId).Take(profile).Error
+}
