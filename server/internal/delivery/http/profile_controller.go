4eb2ee1a317722dd616321dd5a8cfce49a329e46
Artt Vincii <otong123321@gmail.com>
2026-01-22 01:09:19 +0700

    Profile Controller

--- server/internal/delivery/http/profile_controller.go
+++ server/internal/delivery/http/profile_controller.go
@@ -1,7 +1,10 @@
 package http
 
 import (
+	"github.com/gofiber/fiber/v2"
 	"github.com/sirupsen/logrus"
+	"tratech.my.id/server/internal/delivery/http/middleware"
+	"tratech.my.id/server/internal/model"
 	"tratech.my.id/server/internal/usecase"
 )
 
@@ -15,4 +18,24 @@ func NewProfileController(useCase *usecase.ProfileUseCase, log *logrus.Logger) *
 		UseCase: useCase,
 		Log:     log,
 	}
+}
+
+func (c *ProfileController) Create(ctx *fiber.Ctx) error {
+	auth := middleware.GetUser(ctx)
+
+	request := new(model.CreateProfileRequest)
+	if err := ctx.BodyParser(request); err != nil {
+		c.Log.WithError(err).Error("error parsing body request")
+		return fiber.ErrBadRequest
+	}
+
+	request.UserId = auth.ID
+
+	response, err := c.UseCase.Create(ctx.UserContext(), request)
+	if err != nil {
+		c.Log.WithError(err).Error("Error creating profile")
+		return err
+	}
+
+	return ctx.JSON(model.WebResponse[*model.ProfileResponse]{Data: response})
 }
